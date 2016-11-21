60d872ce87f9ff004beff33c9078844a58028f3a
Jouko Saastamoinen <joukosaastamoinen@gmail.com>
2016-11-21 22:46:19 +0200

    Remove Ramda dependency from examples

--- examples/src/weirdo.js
+++ examples/src/weirdo.js
@@ -1,17 +1,21 @@
-const R = require('ramda')
-
 const {constant, sine, triangle, amp} = require('audiate')
 
-module.exports = R.pipe(
-  () => 0.7,
-  constant,
-  sine,
-  amp(constant(1)),
-  sine,
-  amp(constant(10)),
-  sine,
-  amp(constant(5)),
-  sine,
-  amp(constant(220)),
-  triangle
-)
+module.exports = () =>
+  triangle(
+    amp(
+      constant(220),
+      sine(
+        amp(
+          constant(5),
+          sine(
+            amp(
+              constant(10),
+              sine(
+                constant(0.7)
+              )
+            )
+          )
+        )
+      )
+    )
+  )
